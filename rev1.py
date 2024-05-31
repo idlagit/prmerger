3d7e959bf6d6ee0faad91f5dac5d1bd3676f67c9
idlagit <88596820+idlagit@users.noreply.github.com>
2024-05-31 09:22:53 -0400

    Update rev1.py
    add mulitplication function

--- rev1.py
+++ rev1.py
@@ -9,9 +9,13 @@ def sub(num1, num2):
   minus = num1 - num2
   return minus
 
+def mul(num1, num2):
+  times = num1 * num2
+  return times
 
 luckyadd = add(5, 16)
 luckysub = sub(19, 16)
 
 print(luckyno)
 print(luckysub)
+print(times)
